3b9f3eb4e5485886abb9e480704f9f44c169d816
Spacecious <mvzivadinovic@gmail.com>
2025-06-07 19:16:40 +0200

    zoom

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+public class CameraZoom : MonoBehaviour
+{
+    private float zoom;
+    private float zoomMultiplier = 2f;
+    private float minZoom = 2f;
+    private float maxZoom = 4f;
+    private float velocity = 0f;
+    private float smoothTime = 0.25f;
+
+    [SerializeField] private Camera cam;
+
+    private void Start()
+    {
+        zoom = cam.orthographicSize;
+    }
+
+    private void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoom -= scroll * zoomMultiplier;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+    }
+}
+
